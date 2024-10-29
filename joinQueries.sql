19364093c4f748da7b7e152265c64aff9d0fa7f7
Pedro Santos <pedro.o.santos7@aluno.senai.br>
2024-10-29 00:06:00 -0300

    feat: create join queries (probably unfunctional)

---
+++ joinQueries.sql
@@ -0,0 +1,7 @@
+SELECT blablabla.blablabla_2, blablabla.blablabla_3, uiuiui.uiuiui_2  FROM blablabla INNER JOIN uiuiui ON blablabl.uiuiui_ID = uiuiui.uiuiui_ID;
+
+SELECT blablabla.blablabla_2, blablabla.blablabla_3, uiuiui.uiuiui_2  FROM blablabla LEFT JOIN uiuiui ON blablabl.uiuiui_ID = uiuiui.uiuiui_ID;
+
+SELECT blablabla.blablabla_2, blablabla.blablabla_3, uiuiui.uiuiui_2  FROM blablabla RIGHT JOIN uiuiui ON blablabl.uiuiui_ID = uiuiui.uiuiui_ID;
+
+SELECT blablabla.blablabla_2, blablabla.blablabla_3, uiuiui.uiuiui_2  FROM blablabla FULL JOIN uiuiui ON blablabl.uiuiui_ID = uiuiui.uiuiui_ID;
