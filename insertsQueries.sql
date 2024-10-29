7f0d724631527232b389ff7c0f5dd9e6660931c9
Pedro Santos <pedro.o.santos7@aluno.senai.br>
2024-10-29 00:20:55 -0300

    feat: add real content to inserts

--- insertsQueries.sql
+++ insertsQueries.sql
@@ -1,16 +1,16 @@
-INSERT INTO blablabla (blablabla_2, blablabla_3) VALUES
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
-(, '');
+INSERT INTO jogos (jogo_nome, ano_lancamento) VALUES
+('Pokémon: Diamond', 2006);
+('South Park: The Stick of Truth', 2014);
+('God of War', 2018);
+('Naruto Ultimate Ninja Storm', 2008);
+('Naruto Shippuden Ultimate Ninja Storm 2', 2010);
+('Naruto Shippuden Ultimate Ninja Storm 3', 2013);
+('Naruto Shippuden Ultimate Ninja Storm 4', 2016);
+('Mineirinho Ultra Adventures', 2017);
+('Mineirinho Ultra Adventures 2', 2022);
+('Palworld', 2024);
 
 INSERT INTO generos (nome) VALUES
-('');
-('');
-('');
+('Luta');
+('RPG');
+('Aventura');
